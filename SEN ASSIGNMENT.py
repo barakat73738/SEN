d17cd947a20f89756045d3028405a1afa7cdb37d
barakat73738 <Oyewolebarakat68@gmail.com>
2025-10-27 14:31:13 +0100

    Add files via upload

---
+++ SEN ASSIGNMENT.py
@@ -0,0 +1,7 @@
+>> # SEN 201 Task
+>>> # This simple program prints a classic greeting
+>>> def greet():
+...     """Prints the required 'Hello' message."""
+...     print("Hello, World")
+...     # Call the function to run the program
+...     if _name_ == "_main_":
